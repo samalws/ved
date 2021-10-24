8c55eb65778a53cc66bee7daf4c73a255fa9c055
Sam Alws <sam@samalws.com>
2021-10-24 17:42:09 -0400

    got a standard interface thing for commands

--- TextEditor.hs
+++ TextEditor.hs
@@ -1,24 +1,30 @@
 import Safe
 import Data.List.Index
-import Control.Monad.State
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
+import Data.Maybe
+import System.IO
 
 type Line = String
 type Buffer = [Line] -- TODO array
-type EditState a = State Buffer a
+type EditorMonad a = StateT Buffer IO a -- TODO IO??
 type LineNum = Int
 type CharNum = Int
 type Coord = (LineNum, CharNum)
+data LongCmd = LongCmd { runLongCmd :: Char -> Either LongCmd (EditorMonad ()) }
+data CmdState = CmdFail | CmdShort (EditorMonad ()) | CmdLong LongCmd
+type CmdsList = [Char -> CmdState]
 
-getBuffer :: EditState Buffer
+getBuffer :: EditorMonad Buffer
 getBuffer = get
 
-getBufferF :: (Buffer -> a) -> EditState a
+getBufferF :: (Buffer -> a) -> EditorMonad a
 getBufferF = (<$> getBuffer)
 
-modifyBuffer :: (Buffer -> Buffer) -> EditState ()
+modifyBuffer :: (Buffer -> Buffer) -> EditorMonad ()
 modifyBuffer = modify 
 
-setBuffer :: Buffer -> EditState ()
+setBuffer :: Buffer -> EditorMonad ()
 setBuffer = modifyBuffer . const
 
 bufferGetLine :: Int -> Buffer -> Maybe Line
@@ -30,13 +36,34 @@ bufferModifyLine = modifyAt
 bufferSetLine :: Int -> Line -> Buffer -> Buffer
 bufferSetLine = setAt
 
-getLine :: Int -> EditState (Maybe Line)
+getLine :: Int -> EditorMonad (Maybe Line)
 getLine = getBufferF . bufferGetLine
 
-modifyLine :: Int -> (Line -> Line) -> EditState ()
+modifyLine :: Int -> (Line -> Line) -> EditorMonad ()
 modifyLine n f = modifyBuffer $ bufferModifyLine n f
 
-setLine :: Int -> Line -> EditState ()
+setLine :: Int -> Line -> EditorMonad ()
 setLine n l = modifyBuffer $ bufferSetLine n l
 
-main = pure ()
+cmdsList :: CmdsList
+cmdsList = []
+
+editorLongCmdInProgress :: LongCmd -> EditorMonad ()
+editorLongCmdInProgress f = lift getChar >>= (either editorLongCmdInProgress id . runLongCmd f)
+
+editorHelper :: Char -> CmdsList -> EditorMonad ()
+editorHelper c [] = lift $ putStrLn $ " No command found for " <> show c
+editorHelper c (a:b) = case (a c) of
+  CmdFail -> editorHelper c b
+  (CmdShort m) -> m
+  (CmdLong f) -> editorLongCmdInProgress f
+
+editor :: CmdsList -> EditorMonad ()
+editor allCmds = do
+  chr <- lift getChar
+  editorHelper chr allCmds
+  editor allCmds
+
+main = do
+  hSetBuffering stdin NoBuffering
+  runStateT (editor cmdsList) []
